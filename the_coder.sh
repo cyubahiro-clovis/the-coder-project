77fe85dc533addbc67c423db73bb2e3cb271d8de
cyubahiro-clovis <m.clovis@alustudent.com>
2026-05-21 01:44:07 -0700

    Add the_coder.sh script

---
+++ the_coder.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+mkdir -p students_directory
+touch students_directory/Cohort-1.txt
+touch students_directory/Cohort-2.txt
+touch students_directory/Cohort-3.txt
+
+echo "students_directory created with Cohort-1.txt, Cohort-2.txt, and Cohort-3.txt"
